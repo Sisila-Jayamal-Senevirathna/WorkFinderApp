cfc0759fc572c35ae671c8368200e0fe1ffa09fd
Sisila <sisilajayamal1@gmail.com>
2024-05-07 00:43:43 +0530

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, FlatList, Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
+import uuid from "react-native-uuid";
+import Task from './Components/Task';
+
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks]= useState([]);
+
+  const addToList = () => {
+    if (inputValue === "") {
+      Alert.alert("Oops", 'Input is Empty', [
+        {
+          text: 'Ok.',
+        }
+      ]);
+      return;
+    }
+    const newItem = {
+      id: uuid.v4(),
+      item: inputValue,
+    };
+
+    setTasks([newItem, ...tasks]);
+    setInputValue("");
+    Keyboard.dismiss();
+  };
+
+  const deleteFromList=(id)=>{
+    setTasks(tasks.filter((task)=> task.id!=id))
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style ={styles.taskWrapper}>
+
+      <Text style={styles.sectionTitle}>Work Finder</Text>
+      <Text>Welcome to the App!</Text>
+      
+       <TextInput
+        value = {inputValue}
+        style={styles.input}
+        placeholder={"Add a Task...."}
+        onChangeText={(text) => setInputValue(text)}
+      /> 
+      <TouchableOpacity onPress={addToList}>
+        <View style={styles.btn}>
+          <Text style={{color:"white"}}>Add</Text>
+
+          <MaterialIcons 
+          style={{marginLeft:4, color: 'white'}} 
+          name='add-box' 
+          size={24}
+          />
+        </View>
+      </TouchableOpacity>
+
+      <View>
+        <FlatList 
+        keyExtractor={(item)=>item.id}
+        data={tasks}
+        renderItem={({item})=>(          
+          <Task task={item} deleteFromList={deleteFromList}/>
+        )}
+        />
+      </View>
+      </View>
+      <StatusBar style="auto" />      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    backgroundColor: '#E8EAED',       
   },
+
+  taskWrapper: {
+    flex: 1,
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+
+  input:{
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,  
+    marginTop: 10,  
+  },
+
+  sectionTitle:{
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+
+  btn:{
+   padding: 10,
+   borderRadius:5,
+   alignItems: "center",
+   backgroundColor:"#72bcd4",
+   marginTop: 10,
+   flexDirection:"row",
+   justifyContent: "center",
+   marginBottom: 10,
+  },
+
 });
---
+++ Components/Task.js
@@ -0,0 +1,34 @@
+import React from "react";
+import{View,Text,StyleSheet} from 'react-native';
+import {MaterialIcons} from "@expo/vector-icons";
+
+const Task =({task:{id,item}, deleteFromList})=>{
+    return (
+        <View style={styles.item}>
+            <View>
+                <View></View>
+                    <Text>{item}</Text>
+                </View>
+            <MaterialIcons 
+            onPress={()=>deleteFromList(id)} 
+            name='delete' 
+            size={20}
+            color= 'red'
+            />
+        </View>
+    );
+};
+export default Task;
+
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 11,
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        marginBottom: 10,
+        alignItems: "center",
+    },
+});
+
